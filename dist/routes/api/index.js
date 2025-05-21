700aa2a932902ff24c644a5c13612402fa1ec98b
KayBroRob <kaylynbrooke.r@gmail.com>
2025-05-20 22:39:45 -0700

    update1

---
+++ dist/routes/api/index.js
@@ -0,0 +1,7 @@
+import { Router } from 'express';
+const router = Router();
+import userRoutes from './user_routes.js';
+import thoughtsRoutes from './thought_routes.js';
+router.use('/users', userRoutes);
+router.use('/thoughts', thoughtsRoutes);
+export default router;
